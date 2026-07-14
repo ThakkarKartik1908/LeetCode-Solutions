e9fe02a034181bdc634c962ad4b62e4952472b69
Kartik Thakkar <thakkarkartik198@gmail.com>
2026-07-14 22:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (60.18%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void getallsubsets(vector<int>& n,vector<int>& ans,int i,vector<vector<int>>& allsubsets){
+        if(i == n.size()){
+            allsubsets.push_back({ans});
+            return;
+        }
+        //include
+        ans.push_back(n[i]);
+        getallsubsets(n,ans,i+1,allsubsets);
+
+        ans.pop_back();
+
+        //exlude
+        getallsubsets(n,ans,i+1,allsubsets);
+
+        
+    }
+    vector<vector<int>> subsets(vector<int>& n) {
+        vector<vector<int>> allsubsets;
+        vector<int> ans;
+
+        getallsubsets(n,ans,0,allsubsets);
+        return allsubsets;
+    }
+};
